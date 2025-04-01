631fd5b256cf66abb7d2193aa925000b8b7e00e3
sant-kumar <s49111844@gmail.com>
2025-04-01 10:10:48 +0530

    push day1

---
+++ day1/basic.js
@@ -0,0 +1,10 @@
+export const add = (a,b)=>{
+    return a+b;
+}
+export const sub = (a,b)=>{
+    return a-b;
+}
+export const mul = (a,b) =>{
+    return a*b;
+}
+//module.exports = {add,sub,mul};
---
+++ day1/index.js
@@ -0,0 +1,5 @@
+//const calc = require("./basic.js")
+import {add,sub,mul} from "./basic.js";
+console.log(add(3,7));
+console.log(sub(7,3));
+console.log(mul(7,3));
